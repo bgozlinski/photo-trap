0afb69ef88906f5d992c0d4142c7d00b5a6916d3
bgozlinski <bartlomiej.gozlinski@gmail.com>
2022-07-08 13:37:16 +0200

    changes in WIDTH, HEIGHT,DISTANCE_WIDTH, DISTANCE_HEIGHT

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
+"""'320.0x240.0': 'OK'
+'640.0x480.0': 'OK'
+'1280.0x720.0': 'OK'"""
+
 WIDTH = 1280  # camera width
 HEIGHT = 720  # camera height
+
+DISTANCE_WIDTH = 200  # distance from centre in y-axis
+DISTANCE_HEIGHT = 200  # distance from centre in x-axis
--- main.py
+++ main.py
@@ -3,20 +3,15 @@ from camera import Camera
 from draw import Draw
 from config import *
 
-"""'320.0x240.0': 'OK'
-'640.0x480.0': 'OK'
-'1280.0x720.0': 'OK'"""
-
 # initiate camera
 initCamera = Camera(WIDTH, HEIGHT)
 
 while True:
     # get frame
     frame = initCamera.getFrame()
-    # draw rectangle in centre of frame with cW, cH distance from centre.
-    cW, cH = 100, 100
-    Draw(frame).rectangle((int(WIDTH/2-cW), int(HEIGHT/2 - cH)),
-                          (int(cW + WIDTH/2), int(cH + HEIGHT/2)))
+    # draw rectangle in centre of (WIDTH, HEIGHT) with (dW, dH) lengths.
+    Draw(frame).rectangle((int(WIDTH/2-DISTANCE_WIDTH/2), int(HEIGHT/2 - DISTANCE_HEIGHT/2)),
+                          (int(DISTANCE_WIDTH + WIDTH/2), int(DISTANCE_HEIGHT + HEIGHT/2)))
     # show frame on screen
     cv2.imshow("Frame", frame)
 
