93ba5ae392d1b75d4147434cedab09fe502deab6
bgozlinski <bartlomiej.gozlinski@gmail.com>
2022-07-09 03:50:09 +0200

    formatting

--- camera.py
+++ camera.py
@@ -5,6 +5,7 @@ class Camera:
     """
     Class to assign camera.
     """
+
     def __init__(self, width, height):
         self.capture = cv2.VideoCapture(1)
         self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
@@ -15,6 +16,7 @@ class Camera:
     def getFrame(self):
         frame = self.capture.read()[1]
         return frame
+
     # close camera
     def closeCamera(self):
         self.capture.release()
--- main.py
+++ main.py
@@ -5,17 +5,16 @@ from config import *
 
 # initiate camera
 initCamera = Camera(WIDTH, HEIGHT)
-
 while True:
     # get frame
     frame = initCamera.getFrame()
     # draw rectangle in centre of (WIDTH, HEIGHT) with (dW, dH) lengths.
-    Draw(frame).rectangle((int(WIDTH/2-DISTANCE_WIDTH/2), int(HEIGHT/2 - DISTANCE_HEIGHT/2)),
-                          (int(DISTANCE_WIDTH + WIDTH/2), int(DISTANCE_HEIGHT + HEIGHT/2)))
+    Draw(frame).rectangle(
+        (int(WIDTH / 2 - DISTANCE_WIDTH / 2), int(HEIGHT / 2 - DISTANCE_HEIGHT / 2)),
+        (int(DISTANCE_WIDTH + WIDTH / 2), int(DISTANCE_HEIGHT + HEIGHT / 2)),
+    )
     # show frame on screen
     cv2.imshow("Frame", frame)
-
     # press q to end loop
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
-
